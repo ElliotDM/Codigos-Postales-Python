6becaade946d161ccc5b00a48b754998a6f96cb2
ElliotDM <elliotd70@outlook.com>
2022-10-01 19:52:10 -0500

    Modificaciones graficas

--- GUI.py
+++ GUI.py
@@ -7,6 +7,7 @@ class Interface():
     def __init__(self):
         self.root = Tk()
         self.root.title("Codigos Postales")
+        self.root.iconbitmap('icons\\zip-code.ico')
         self.root.resizable(0,0)
 
         self.frame = ttk.Frame(self.root, borderwidth=2, 
@@ -22,19 +23,19 @@ class Interface():
         self.textbox2 = ttk.Entry(self.frame, textvariable=self.cp, width=30)
 
         self.button1 = ttk.Button(self.frame, text='Buscar', padding=(5,5), command=self.search)
-        self.button2 = ttk.Button(self.frame, text='Siguiente', padding=(5,5))
 
+        self.label3 = ttk.Label(self.frame, text='Previsualizacion', padding=(5,5))
         self.textarea = Text(self.frame, height=10, width=30, padx=5, pady=5, state=DISABLED)
 
-        self.button3 = ttk.Button(self.frame, text='Crear archivo', padding=(5,5), command=self.choose_dir)
+        self.button3 = ttk.Button(self.frame, text='Guardar', padding=(5,5), command=self.choose_dir)
 
         self.frame.grid(column=0, row=0)
         self.label1.grid(column=0, row=0)
-        self.textbox1.grid(column=1,row=0, columnspan=2)
+        self.textbox1.grid(column=1,row=0, columnspan=3)
         self.label2.grid(column=0, row=1)
-        self.textbox2.grid(column=1, row=1, columnspan=2)
-        self.button1.grid(column=1,row=3)
-        self.button2.grid(column=2,row=3)
+        self.textbox2.grid(column=1, row=1, columnspan=3)
+        self.button1.grid(column=2,row=3)
+        self.label3.grid(column=0, row=4)
         self.textarea.grid(column=0, row=5, columnspan=3)
         self.button3.grid(column=1, row=7)
 
@@ -42,6 +43,10 @@ class Interface():
         self.root.mainloop() 
 
     def search(self):
+        if not self.check_values():
+            messagebox.showerror('Error', 'Ingrese sus datos')
+            return
+
         data = get_data(self.cp.get(), make_dict())
         colonia = data.get('d_asenta')
         estado = data.get('d_estado')
@@ -63,6 +68,10 @@ class Interface():
         self.textarea.configure(state=DISABLED)
 
     def choose_dir(self):
+        if not self.check_values():
+            messagebox.showerror('Error', 'Ingrese sus datos')
+            return
+
         parent_dir = filedialog.askdirectory()
         data = get_data(self.cp.get(), make_dict())
         var = check_directory(self.cp.get(), parent_dir)
@@ -72,6 +81,9 @@ class Interface():
         else:
             messagebox.showwarning("Atencion", var)
             make_file(self.name.get(), self.cp.get(), data, parent_dir)
+
+    def check_values(self):
+        return len(self.name.get())>0 and len(self.cp.get())>0
 
 
 def main():
