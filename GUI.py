196f77e2b4bb002c323038e8cf7d8788547fe4e6
ElliotDM <elliotd70@outlook.com>
2022-10-02 18:11:15 -0500

    Busqueda de colonia incluida

--- GUI.py
+++ GUI.py
@@ -1,17 +1,18 @@
 from tkinter import *
 from tkinter import Tk, ttk, StringVar, Text, filedialog, messagebox
+from tkinter import font
 from main import get_data, make_dict, make_file, check_directory
 
 
-class Interface():
+class Interface(ttk.Frame):
     def __init__(self):
         self.root = Tk()
-        self.root.title("Codigos Postales")
+        self.root.title('Codigos Postales')
         self.root.iconbitmap('icons\\zip-code.ico')
         self.root.resizable(0,0)
 
         self.frame = ttk.Frame(self.root, borderwidth=2, 
-                    relief="raised", padding=(10,10))
+                    relief="raised", padding=(5,5))
 
         self.label1 = ttk.Label(self.frame, text='Nombre:', padding=(5,5))
         self.label2 = ttk.Label(self.frame, text='C.P.:', padding=(5,5))
@@ -22,9 +23,14 @@ class Interface():
         self.textbox1 = ttk.Entry(self.frame, textvariable=self.name, width=30)
         self.textbox2 = ttk.Entry(self.frame, textvariable=self.cp, width=30)
 
-        self.button1 = ttk.Button(self.frame, text='Buscar', padding=(5,5), command=self.search)
+        self.label3 = ttk.Label(self.frame, text='Colonia: ', padding=(5,5))
+        self.combobox = ttk.Combobox(self.frame, width=27, state="readonly")
 
-        self.label3 = ttk.Label(self.frame, text='Previsualizacion', padding=(5,5))
+        self.button1 = ttk.Button(self.frame, text='Buscar', padding=(5,5), command=self.search)
+        self.button2 = ttk.Button(self.frame, text='Aceptar', padding=(5,5), command=self.make_selection)
+
+        self.label4 = ttk.Label(self.frame, text='Previsualizacion', padding=(5,5))
+        
         self.textarea = Text(self.frame, height=10, width=30, padx=5, pady=5, state=DISABLED)
 
         self.button3 = ttk.Button(self.frame, text='Guardar', padding=(5,5), command=self.choose_dir)
@@ -34,10 +40,13 @@ class Interface():
         self.textbox1.grid(column=1,row=0, columnspan=3)
         self.label2.grid(column=0, row=1)
         self.textbox2.grid(column=1, row=1, columnspan=3)
-        self.button1.grid(column=2,row=3)
-        self.label3.grid(column=0, row=4)
-        self.textarea.grid(column=0, row=5, columnspan=3)
-        self.button3.grid(column=1, row=7)
+        self.label3.grid(column=0, row=3)
+        self.combobox.grid(column=1, row=3, columnspan=3)
+        self.button1.grid(column=1,row=4)
+        self.button2.grid(column=2,row=4)
+        self.label4.grid(column=1, row=5)
+        self.textarea.grid(column=0, row=6, columnspan=3)
+        self.button3.grid(column=1, row=8)
 
         self.textbox1.focus_set()
         self.root.mainloop() 
@@ -48,15 +57,25 @@ class Interface():
             return
 
         data = get_data(self.cp.get(), make_dict())
-        colonia = data.get('d_asenta')
-        estado = data.get('d_estado')
-        municipio = data.get('d_mnpio')
-        ciudad = data.get('d_ciudad')
+        self.combobox.config(postcommand=self.add_options(data))
+        
+
+    def make_selection(self):
+        data = get_data(self.cp.get(), make_dict())
+        
+        for option in data:
+            if option.get('d_asenta') == self.combobox.get():
+                selection = option
+
+        colonia = selection.get('d_asenta')
+        estado = selection.get('d_estado')
+        municipio = selection.get('d_mnpio')
+        ciudad = selection.get('d_ciudad')
 
         self.textarea.configure(state=NORMAL)
         self.textarea.delete('1.0', END)
     
-        if data.get('d_ciudad') == '':
+        if selection.get('d_ciudad') == '':
             self.textarea.configure(state=NORMAL)
             self.textarea.insert('1.0', f'Nombre: {self.name.get()}\nC.P.: {self.cp.get()}\nColonia: {colonia}\nEstado: {estado}\nMunicipio: {municipio}')
             self.textarea.configure(state=DISABLED)
@@ -66,6 +85,10 @@ class Interface():
         self.textarea.configure(state=NORMAL)
         self.textarea.insert('1.0', f'Nombre: {self.name.get()}\nC.P.: {self.cp.get()}\nColonia: {colonia}\nEstado: {estado}\nMunicipio: {municipio}\nCiudad: {ciudad}')
         self.textarea.configure(state=DISABLED)
+        
+    def add_options(self, data:list):
+        colonias = [data[idx].get('d_asenta') for idx in range(len(data))]
+        self.combobox['values'] = colonias
 
     def choose_dir(self):
         if not self.check_values():
@@ -73,13 +96,12 @@ class Interface():
             return
 
         parent_dir = filedialog.askdirectory()
-        data = get_data(self.cp.get(), make_dict())
-        var = check_directory(self.cp.get(), parent_dir)
+        data = self.textarea.get('1.0', END)
         
-        if var == '':
+        if not check_directory(self.cp.get(), parent_dir):
+            messagebox.showwarning('Atencion', f'El directorio {self.cp.get()} ya existe')
             make_file(self.name.get(), self.cp.get(), data, parent_dir)
         else:
-            messagebox.showwarning("Atencion", var)
             make_file(self.name.get(), self.cp.get(), data, parent_dir)
 
     def check_values(self):
@@ -90,5 +112,5 @@ def main():
     app = Interface()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -15,42 +15,34 @@ def make_dict():
     return [dict(zip(key_list, value_list[i])) for i in range(len(lines))]
 
 
-def get_data(cp:str, dict_list:list) -> dict:   
+def get_data(cp:str, dict_list:list) -> list:   
+    similarly = []
+
     for idx in range(len(dict_list)):
         aux : dict = dict_list[idx]
         if cp == aux.get('d_codigo'):
-            break
-       
-    return dict_list[idx]
+            similarly.append(aux)
+        else:
+            continue
+
+    return similarly
 
 
-def make_file(name: str, cp: str, data: dict, directory: str):
+def make_file(name: str, cp: str, data: str, directory: str):
     parent_dir = path.join(directory, cp)
-    
     file_name = f'{parent_dir}\\{name}.txt'
-    col = data.get('d_asenta')
-    est = data.get('d_estado')
-    mun = data.get('d_mnpio')
      
     with open(file_name, 'w', encoding='UTF-8') as f:
-        f.write(f'Nombre: {name}\n')
-        f.write(f'C.P.: {cp}\n')
-        f.write(f'Colonia: {col}\n')
-        f.write(f'Estado: {est}\n')
-        f.write(f'Municipio: {mun}\n')
-        
-        if data.get('d_ciudad'):
-            cd = data.get('d_ciudad')
-            f.write(f'Ciudad: {cd}\n')
+        f.write(f'{data}')
 
 
 def check_directory(cp:str, parent_dir: str):
     directory = path.join(parent_dir, cp)
-    message = ''
+    aux = True
 
     try:
         mkdir(directory)
     except OSError:
-        message = f"El directorio {cp} ya existe"
+        aux = False
 
-    return message 
+    return aux
